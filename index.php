332fadb73692aa839ef7f2eb770209282835f05e
Mạnh Nghĩa <bmn.2312@gmail.com>
2016-12-14 21:41:18 +0700

    1

--- index.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-require_once 'src/TesseractOCR/TesseractOCR.php';
-//or require_once 'vendor/autoload.php' if you are using composer
-
-$tesseract = new TesseractOCR();
-$tesseract->setImage('tests/images/hello.png');
-echo $tesseract->recognize();
-?>
