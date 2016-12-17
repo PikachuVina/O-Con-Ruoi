6c8c61b189b325f452bd4e166c5960554380af71
Mạnh Nghĩa <bmn.2312@gmail.com>
2016-12-17 23:35:46 +0700

    1

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 ﻿<meta charset="utf-8">
-<?php
-echo '{</br>
-  "message": "Chào ấy ! Mình là BMN2312 :D", </br>
-  "status": 403</br>
-}';
-?>
+<title>BMN2312</title>
+Chào ấy !!! Mình là BMN2312 :D
