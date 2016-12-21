691685c4fe432af761c6e846e012b016a231f3cc
Mạnh Nghĩa <bmn.2312@gmail.com>
2016-12-21 21:19:10 +0700

    1

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
 <?php
-ini_set('session.gc_maxlifetime', 1*60*60);
-SESSION_START();
-// config database
-$host = "mysql3.gear.host";
-$username = "nghia";
-$password = "fujn#Lkf349@uOm!";	
-$dbname = "nghia";
-$connection = mysql_connect($host,$username,$password);
-if (!$connection)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
-mysql_select_db($dbname) or die(mysql_error());
-mysql_query("SET NAMES utf8");
-?>
+echo 'HiHi';
